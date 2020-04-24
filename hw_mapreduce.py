b27d3d5aeff850739d69edf6b11b276aeb701358
ADREF Brahim <adrefbrahim@gmail.com>
2020-04-24 19:09:49 +0200

    MapReduce other examples

--- hw_mapreduce.py
+++ hw_mapreduce.py
@@ -67,6 +67,38 @@ the main steps :
 
 '''
 
+# the MapReduce is a good strategy, but it isn't always easy to reformulate 
+# It's even sometimes impossible  !!!!
+
+# To familiarize with the MapReduce logique we go to implement others examples 
+
+'''
+First Example: Multiplication (Matrix * Vector) for the famous alogrithm PageRank used to schedule 
+              the results of a research web
+
+steps : 
+    
+    - Matrix is stocked as a triplet (i, j, M[i][j]) and we don't represent the 0's positions 
+    - The same thing for the Vector V (i, V[i])
+    - here we have 2 cases:
+    
+        ./ Case 1: V is small enough ro fit in the memory of the Map node
+            
+            * Map(key, value) will give us the peers (i, M[i][j] * V[i])
+                SHUFFLE & SORT : Groups all values assosicieted with the same key i in a pair (i, [v1, v2, ...])
+            * Reduce(key, value) will sum all vaulues with a gaven key
+        
+        ./ Case 2: V is too large to fit in the memory of the Map node 
+        
+            Here we have to split V to horizontal stripes and M to virticla stripes and we apply the same strategy
+'''
+
+
+
+   
+
+
+
 
 
 
